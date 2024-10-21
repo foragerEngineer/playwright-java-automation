a43fe16de286f2fe2f0a51f7b89f232cff32a1af
foragerEngineer <mikel.tan86@gmail.com>
2024-10-21 13:38:32 -0700

    driver setup still needs to be worked out

--- src/test/java/m/sierra/Tests/BaseTest.java
+++ src/test/java/m/sierra/Tests/BaseTest.java
@@ -1,4 +1,26 @@
 package m.sierra.Tests;
 
+import com.microsoft.playwright.Page;
+import m.sierra.Driver.PlaywrightFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 public class BaseTest {
+
+    protected Page page;
+    PlaywrightFactory driver;
+
+    @BeforeClass
+    @Parameters({"appURL", "browserType"})
+    public void setUp(String appURL, String browserType) {
+        driver = new PlaywrightFactory();
+        page = driver.getPage(appURL, browserType);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        page.context().browser().close();
+    }
+
 }
