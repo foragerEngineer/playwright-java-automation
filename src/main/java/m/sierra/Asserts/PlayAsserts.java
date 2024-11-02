91d5c223dae135c5c17bbc6e0a3058e3500f35f3
foragerEngineer <124650236+foragerEngineer@users.noreply.github.com>
2024-11-02 10:14:08 -0700

    added allure reports to gitignore and did some formatting (#2)

--- src/main/java/m/sierra/Asserts/PlayAsserts.java
+++ src/main/java/m/sierra/Asserts/PlayAsserts.java
@@ -13,6 +13,11 @@ public class PlayAsserts {
         this.page = page;
     }
 
+    /***
+     * Asserts that the element is visible by using the locator HTML tag
+     * @param elementLocator provide an HTML tag locator
+     * @param text provide the text to be asserted
+     */
     public void assertElementHasText(Locator elementLocator, String text) {
         assertThat(elementLocator).hasText(text);
     }
