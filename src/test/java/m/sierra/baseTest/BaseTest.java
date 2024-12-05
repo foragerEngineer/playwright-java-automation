27925e6a2f7af5361c4e337b5511c06ffbd423ee
foragerEngineer <mikel.tan86@gmail.com>
2024-12-05 00:21:52 -0800

    slack configuration working

--- src/test/java/m/sierra/baseTest/BaseTest.java
+++ src/test/java/m/sierra/baseTest/BaseTest.java
@@ -2,26 +2,33 @@ package m.sierra.baseTest;
 
 import m.sierra.Driver.PlaywrightFactory;
 import m.sierra.Pages.BasePage;
+import m.sierra.SlackReportSender.SlackReportUtil;
 import m.sierra.Utilities.BrowserUtils;
+import m.sierra.utilities.listeners.LogListener;
+import org.testng.ITestContext;
 import org.testng.annotations.*;
 
+@Listeners(LogListener.class)
 public class BaseTest extends PlaywrightFactory {
+
+    SlackReportUtil slackReportUtil = new SlackReportUtil();
+    LogListener logListener = new LogListener();
 
     @BeforeClass
     @Parameters({"appURL", "browserType"})
     public void setUp(@Optional("https://playwright.dev/") String appURL,
-                      @Optional("chrome") String browserType) {
+                      @Optional("chrome") String browserType, ITestContext context) {
+        logListener.onStart(context);
         page = getPage(appURL, browserType);
         BasePage.setAndConfigurePage(page);
         BrowserUtils browserUtils = new BrowserUtils(page.context().browser());
         browserUtils.newPageContext()
                 .startVideoRecording();
+        slackReportUtil.setupParentMessage();
     }
 
-
-
     @AfterClass
-    public void tearDown() {
+    public void tearDown(ITestContext context) {
 //        takeScreenshot();
         if (page != null) {
             page.context().browser().close();
@@ -29,6 +36,8 @@ public class BaseTest extends PlaywrightFactory {
         if (playwright != null) {
             playwright.close();
         }
+        logListener.onFinish(context);
+        slackReportUtil.sendTestResults(context);
     }
 
 }
